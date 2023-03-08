d8ae25803b79d18e631e5fd2987ef7dd176c98fa
versalitas <caprichismo@gmail.com>
2023-03-08 16:01:09 +0100

    correction of typos

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,39 +4,36 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-    "time"
+	"time"
 )
 
 func Hello(name string) (string, error) {
-	
+
 	//if no name return error
 	if name == "" {
 		return "", errors.New("Empty name")
 	}
-	
+
 	//else return randomized greeting message and nil
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
-//randomformat returns a randomly selected greeting
-func randomFormat() string {
-	//declare formats, slice of greetings
-	formats :=[string]{
-		"Hi there, %v. Welcome",
-		"Great to see you, %v!",
-        "Hail, %v! Well met!",
-     }
-
-// init sets initial values for variables used in the function.
+// initates values for variables used in the function.
 func init() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
+// randomformat returns a randomly selected greeting
+func randomFormat() string {
+	//declare formats, slice of greetings
+	formats := []string{
+		"Hi there, %v. Welcome",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
 
- //return randomly selected message format
- //randomly generated index based on slice's length
- return formats[rand.Intn(len(formats))]
-
- } 
- 
+	//return randomly selected message format
+	//randomly generated index based on slice's length
+	return formats[rand.Intn(len(formats))]
+}
