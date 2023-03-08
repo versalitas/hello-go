60de10b240ee5dd3e21022fb28ffdf12339011b1
versalitas <caprichismo@gmail.com>
2023-03-08 15:56:31 +0100

    adds randomized greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,40 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+    "time"
 )
 
 func Hello(name string) (string, error) {
+	
+	//if no name return error
 	if name == "" {
 		return "", errors.New("Empty name")
 	}
-	message := fmt.Sprintf("Hello %v. Welcome!", name)
+	
+	//else return randomized greeting message and nil
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
+
+//randomformat returns a randomly selected greeting
+func randomFormat() string {
+	//declare formats, slice of greetings
+	formats :=[string]{
+		"Hi there, %v. Welcome",
+		"Great to see you, %v!",
+        "Hail, %v! Well met!",
+     }
+
+// init sets initial values for variables used in the function.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+
+ //return randomly selected message format
+ //randomly generated index based on slice's length
+ return formats[rand.Intn(len(formats))]
+
+ } 
+ 
